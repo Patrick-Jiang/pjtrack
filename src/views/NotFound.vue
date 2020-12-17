bdce4a1d81ba2fc61753617880be3c05bdbe4f5f
patrickjiang <patrick.jiang86@gmail.com>
2020-12-17 14:50:26 -0500

    add 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>404
+    <div>
+      <router-link to="/">Go to homepage</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
