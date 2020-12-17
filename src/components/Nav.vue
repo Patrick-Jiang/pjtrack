dc1b4d883b83e212f818c4f7086c3b1ed6a83c67
patrickjiang <patrick.jiang86@gmail.com>
2020-12-17 18:37:35 -0500

    add svg-spirte-loader

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="nav">
-    <router-link to="/money">Track</router-link>
+    <router-link to="/money">
+      <svg >
+        <use xlink:href="#money"></use>
+      </svg>
+      Tags</router-link>
     |
-    <router-link to="/labels">Tags</router-link>
+    <router-link to="/labels">Track</router-link>
     |
     <router-link to="/statistics">Statistics</router-link>
   </div>
 </template>
 
 <script lang="ts">
+const  importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+try{
+  importAll(require.context('../assets/icons',true, /\.svg$/ ));
+}
+catch(error){
+  console.log(error);
+}
+
+
 export default {
   name: 'Nav'
 };
