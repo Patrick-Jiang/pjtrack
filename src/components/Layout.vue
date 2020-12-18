63329011512276b5c65241317b1676b4ec5f7912
patrickjiang <patrick.jiang86@gmail.com>
2020-12-17 19:38:37 -0500

    add Nav styles

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="nav-wrapper">
     <div class="content">
-      <slot></slot>
+      <slot/>
     </div>
     <Nav/>
   </div>
 </template>
+
 
 <script lang="ts">
 export default {
@@ -15,14 +16,11 @@ export default {
 
 <style lang="scss" scoped>
 .nav-wrapper {
-  border: 1px solid green;
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
-
 .content {
-  border: 1px solid blue;
   overflow: auto;
   flex-grow: 1;
 }
