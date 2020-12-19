509a0831625426b99e61d69a02f50f200bf49a54
patrickjiang <patrick.jiang86@gmail.com>
2020-12-18 21:58:55 -0500

    add EditLabel.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,19 +10,7 @@ import EditLabel from '@/views/EditLabel.vue';
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-    // {
-    //     path: '/',
-    //     name: 'Home',
-    //     component: Home
-    // },
-    // {
-    //     path: '/about',
-    //     name: 'About',
-    //     // route level code-splitting
-    //     // this generates a separate chunk (about.[hash].js) for this route
-    //     // which is lazy-loaded when the route is visited.
-    //     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    // }
+
     {
         path: '/',
         redirect: '/money'
@@ -40,7 +28,7 @@ const routes: Array<RouteConfig> = [
         component: Statistics
     },
     {
-        path: '/labels/edit',
+        path: '/labels/edit/:id',
         component: EditLabel
     },
     {
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,16 +1,27 @@
 <template>
-<Layout>
-  edit
-</Layout>
+  <Layout>
+    edit
+  </Layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Component from 'vue-class-component'
+import Component from 'vue-class-component';
+import {tagListModel} from '@/models/tagListModel';
 
 @Component
 export default class EditLabel extends Vue {
-  
+  created() {
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    const tag = tags.filter(t => t.id === id)[0]
+    if(tag){
+      console.log(tag);
+    }else{
+      this.$router.replace('/404')
+    }
+  }
 }
 
 </script>
